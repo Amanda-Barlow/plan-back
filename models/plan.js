fd61052680989fa3d9345ced50dda409e17b2491
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-06-16 23:58:12 -0800

    styling

--- models/plan.js
+++ models/plan.js
@@ -6,9 +6,9 @@ const PlanSchema = new mongoose.Schema({
   name: { type: String },
   goal: { type: String },
   skills: { type: String },
-//   frequency: { type: String },
-//   coping: { type: String },
-//   contacts: { type: String },
+  frequency: { type: String },
+  coping: { type: String },
+  contacts: { type: String },
 }, { timestamps: true });
 
 const Plan = mongoose.model('Plan', PlanSchema);
